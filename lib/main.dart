8028cd0a855b82f541e817d1f0783000084455c7
Sahil Shrestha <super.sahil.99@gmail.com>
2023-06-04 20:04:09 +1000

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
             // print(user);
             // if (user?.emailVerified ?? false) {
             //   return const Text('Done');
